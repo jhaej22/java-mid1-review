ddd13a67eb0336fe39c14b297b41fbd8614814b9
haejoon <jhaej0722@gmail.com>
2024-07-04 12:00:08 +0900

    commit and push

---
+++ src/lang/LangMain.java
@@ -0,0 +1,20 @@
+package lang;
+
+import java.lang.System;
+// java.lang 패키지는 모든 자바 애플리케이션에 자등으로 import 된다.
+
+public class LangMain {
+
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
+}
+
+/**
+ * java.lang 패키지의 대표적인 클래스들
+ * Object : 모든 자바 객체의 부모 클래스
+ * String: 문자열 클래스
+ * Interger, Long, Double : 래퍼 타입, 기본형 데이터 타입을 객체로 만든 것
+ * Class: 클래스 메타 정보
+ * System: 시스템과 관련된 기본 기능들을 제공
+ */
